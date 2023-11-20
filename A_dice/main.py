d1568a0d3009ab543043195834d2dc517f24ece4
胡智涵 <huzhihan@huzhihandeMacBook-Air.local>
2023-11-20 16:27:53 +0800

    owo

---
+++ A_dice/main.py
@@ -0,0 +1,8 @@
+def print_screen():
+    
+
+def main():
+
+
+if __name__=="__main__":
+    main()
--- C_robot/main.py
+++
@@ -1,8 +0,0 @@
-def print_screen():
-    
-
-def main():
-
-
-if __name__=="__main__":
-    main()
